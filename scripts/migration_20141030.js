5532c482cbccc7aff0859e23438401ac30baba10
zhenkunou <zhenkunou@cloud3edu.com>
2014-10-31 11:19:03 +0800

    refactor: migration

--- scripts/migration_20141030.js
+++ scripts/migration_20141030.js
@@ -1,3 +1,12 @@
 //删除categories和classes的name_1索引
 db.categories.dropIndex('name_1');
 db.classes.dropIndex('name_1');
+
+//move slides to files for lectures
+db.lectures.find().forEach(function(lecture) {
+  lecture.files = [{
+    fileName: 'PPT 1',
+    fileContent: lecture.slides,
+  }];
+  db.lectures.update({_id: lecture._id}, lecture);
+});
