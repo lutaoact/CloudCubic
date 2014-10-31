2a29f9c533151b15a9ebe6fcb0a89d141b8a24ae
zhenkunou <zhenkunou@cloud3edu.com>
2014-10-31 10:40:11 +0800

    chore: lecture files fileName wording

--- scripts/move_slides_to_files_for_lectures.js
+++ scripts/move_slides_to_files_for_lectures.js
@@ -1,6 +1,6 @@
 db.lectures.find().forEach(function(lecture) {
   lecture.files = [{
-    fileName: 'file 1',
+    fileName: 'PPT 1',
     fileContent: lecture.slides,
   }];
   db.lectures.update({_id: lecture._id}, lecture);
