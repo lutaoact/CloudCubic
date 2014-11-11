a26c80a4a51f845239fcaf35a7a9a1babb137bb1
pengchengbi <pengchengbi@cloud3edu.com>
2014-11-11 13:55:56 +0800

    update migration script

--- scripts/migration_20141111.js
+++ scripts/migration_20141111.js
@@ -6,8 +6,15 @@ db.users.dropIndex("username_1");
 //move user username to email if email is undefined
 db.users.find().forEach(function(user) {
   var noEmail = user.email == null || user.email.indexOf('@') == -1;
+  var needUpdate = false;
   if (noEmail) {
     user.email = user.username;
-    db.users.update({_id: user._id}, user);
+    needUpdate = true;
   };
+  if (user.status == null) {
+    user.status = 1;
+    needUpdate = true;
+  }
+  if (needUpdate)
+    db.users.update({_id: user._id}, user);
 });
