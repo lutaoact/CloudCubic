d38629f98d8461382c0b6339c6d9f4ba84ed0ce9
pengchengbi <pengchengbi@cloud3edu.com>
2014-11-18 11:43:01 +0800

    add PATCH access control

--- server/config/express.js
+++ server/config/express.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
     //CORS middleware
     var allowCrossDomain = function(req, res, next) {
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTION');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTION,PATCH');
       res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
 
       next();
