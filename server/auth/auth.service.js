5031dbd8644e1d01c189ab67340efc058953318e
pengchengbi <pengchengbi@cloud3edu.com>
2014-11-19 10:26:54 +0800

    change token expire time to 7 days

--- server/auth/auth.service.js
+++ server/auth/auth.service.js
@@ -57,7 +57,7 @@ function hasRole(roleRequired) {
  * Returns a jwt token signed by the app secret
  */
 function signToken(id, role) {
-  return jwt.sign({ _id: id, role: role}, config.secrets.session, { expiresInMinutes: 60*5 });
+  return jwt.sign({ _id: id, role: role}, config.secrets.session, { expiresInMinutes: 60*24*7 });
 }
 
 /**
