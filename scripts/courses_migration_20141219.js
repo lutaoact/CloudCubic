2e128ded93b538d1eb5996f22b46f0e44f1a4b0a
taolu <taolu@cloud3edu.com>
2014-12-16 10:12:53 +0800

    courses_migration_20141219.js

---
+++ scripts/courses_migration_20141219.js
@@ -0,0 +1,6 @@
+//courses数据迁移：将course的orgId设置为相应category的orgId
+db.courses.find().forEach(function(course) {
+  var category = db.categories.findOne({_id: course.categoryId});
+  print(category.orgId);
+  db.courses.update({_id: course._id}, {$set: {orgId: category.orgId}});
+});
