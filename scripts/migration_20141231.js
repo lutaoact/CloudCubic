8101279051d6d20c4f775b73b617fe9cfd34f618
taolu <taolu@cloud3edu.com>
2014-12-19 14:14:42 +0800

    migration_20141231: dis_topics {$set: {viewersNum: disTopic.viewers.length}}

--- scripts/migration_20141231.js
+++ scripts/migration_20141231.js
@@ -48,3 +48,8 @@ db.courses.find().forEach(function(course) {
 
 // 删除所有未包含orgId的forum
 db.forums.remove({orgId: null})
+
+//设置dis_topics的viewersNum字段，让其值等于viewers字段的元素个数
+db.dis_topics.find().forEach(function(disTopic) {
+  db.dis_topics.update({_id: disTopic._id}, {$set: {viewersNum: disTopic.viewers.length}});
+});
